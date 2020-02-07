74b38ccf80221d5724aa21da3148899a8793365f
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-07 03:16:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,22 +7,10 @@ class School
     @roster = {}
   end
   
-  def add_student(name, grade)
-    @student_name = name
+  def add_student (name, grade)
+    @add_student = name
     @grade = grade
-      if @roster.include?(grade) == false
-        @roster[grade] = []
-      end
-    @roster[grade] << name
   end
-
-  def grade(number)
-    @roster[number]
-  end
-
-  def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-    end
-end
+  
+ 
 end
